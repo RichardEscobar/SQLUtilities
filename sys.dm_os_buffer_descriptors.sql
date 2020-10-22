0ee33fb02071be7b1a75c5c7bcb27d4ba0df226a
Richard Escobar <richard.t.escobar@accenture.com>
2020-10-22 12:39:52 -0400

    new script for memory use
    10/22/2020 :: script to pull the memory use per database

---
+++ sys.dm_os_buffer_descriptors.sql
@@ -0,0 +1,18 @@
+--============================================--
+--author:   richard.t.escobar
+--desc:     
+--changes:
+--============================================--
+use [master]
+go
+
+
+SELECT
+[DatabaseName] = CASE [database_id] WHEN 32767
+THEN 'Resource DB'
+ELSE DB_NAME([database_id]) END,
+COUNT_BIG(*) [Pages in Buffer],
+COUNT_BIG(*)/128 [Buffer Size in MB]
+FROM sys.dm_os_buffer_descriptors
+GROUP BY [database_id]
+ORDER BY [Pages in Buffer] DESC;
