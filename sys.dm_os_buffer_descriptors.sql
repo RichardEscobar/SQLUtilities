21e79e3b2e23c913057a51ed8072ae38dc099d75
Richard Escobar <richard.t.escobar@accenture.com>
2020-10-22 13:14:36 -0400

    Update sys.dm_os_buffer_descriptors.sql
    10/22/2020 1315 hrs. added in some details on what is being done here.

--- sys.dm_os_buffer_descriptors.sql
+++ sys.dm_os_buffer_descriptors.sql
@@ -1,6 +1,10 @@
 --============================================--
---author:   richard.t.escobar
---desc:     
+--author:    richard.t.escobar
+--desc:      When a data page is read from disk, the page is copied into the SQL Server buffer pool and cached for reuse. 
+--           Each cached data page has one buffer descriptor. 
+--           Buffer descriptors uniquely identify each data page that is currently cached in an instance of SQL Server. 
+--           sys.dm_os_buffer_descriptors returns cached pages for all user and system databases. 
+--           This includes pages that are associated with the Resource database.
 --changes:
 --============================================--
 use [master]
@@ -8,11 +12,13 @@ go
 
 
 SELECT
-[DatabaseName] = CASE [database_id] WHEN 32767
-THEN 'Resource DB'
-ELSE DB_NAME([database_id]) END,
+[DatabaseName] = CASE [database_id] WHEN 32767 THEN 'Resource DB' ELSE DB_NAME([database_id]) END,
 COUNT_BIG(*) [Pages in Buffer],
 COUNT_BIG(*)/128 [Buffer Size in MB]
+--select top 10 * 
 FROM sys.dm_os_buffer_descriptors
 GROUP BY [database_id]
 ORDER BY [Pages in Buffer] DESC;
+
+
+
